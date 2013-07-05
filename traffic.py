cc410f3432cf0219d2be6e532ecf562919898c2a
brianhouse <brian.house@gmail.com>
2013-07-05 14:00:15 -0400

    added mapquest traffic, but their images are corrupt :(

---
+++ traffic.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import json, model
+from housepy import config, log, net
+from PIL import Image
+
+log.info("////////// traffic //////////")
+
+lon, lat = -74.0780210, 42.3187870 
+
+# try:
+source = "http://www.mapquestapi.com/traffic/v2/flow?key=%s&mapLat=%s&mapLng=%s&mapHeight=400&mapWidth=400&mapScale=433343" % (config['mapquest'], lat, lon)
+log.debug(source)
+response = net.grab(source, "traffic.gif")
+
+image = Image.open("traffic.gif")
+print(image.info)
+print(dir(image))
+image.show()
+
+
+# except Exception as e:
+#     log.error(log.exc(e))
+#     exit()
+
